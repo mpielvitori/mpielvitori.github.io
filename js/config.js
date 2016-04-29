1d73aa42b0768ccb20638f1787da8e0a13ba917e
mpielvitori <martinpielvitori@gmail.com>
2016-04-28 23:14:00 -0300

    Prueba de parallax cv

--- js/config.js
+++ js/config.js
@@ -6,92 +6,117 @@
 var skillData = {
 	skill1: [
 		{
-			title: "Adobe&nbsp;CS 90%",
-			color: "#f3377a",
-			border: "4px solid #f4709f",
-			value: '90%'
-		},
-		{
-			title: "UI/UX Design 100%",
+			title: "JAVA - Struts2 - Spring - Hibernate 100%",
 			color: "#f3377a",
 			border: "4px solid #f4709f",
 			value: '100%'
 		},
 		{
-			title: "Webtypography 80%",
+			title: "XML / JSON / HTML / CSS 75%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '75%'
+		},		
+		{
+			title: "JS / JQuery / Angular / Node 80%",
 			color: "#f3377a",
 			border: "4px solid #f4709f",
 			value: '80%'
 		},
 		{
-			title: "Logodesign 60%",
+			title: "SQL / NoSQL 70%",
 			color: "#f3377a",
 			border: "4px solid #f4709f",
-			value: '60%'
+			value: '70%'
 		},
-    {
-			title: "3D Max 75%",
+	    {
+			title: "Android / Ionic 40%",
+			color: "#f3377a",
+			border: "4px solid #f4709f",
+			value: '40%'
+		},
+		{
+			title: "OOP / EDA / JMS / SOA 75%",
 			color: "#f3377a",
 			border: "4px solid #f4709f",
 			value: '75%'
 		}
+		
 	],
 	skill2: [
 		{
-			title: "HTML5/CSS3 90%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '90%'
-		},
-		{
-			title: "PHP 75%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '75%'
-		},
-		{
-			title: "MySQL 90%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '90%'
-		},
-		{
-			title: "JS/Jquery 60%",
+			title: "Agile/Scrum 60%",
 			color: "#1b7ee4",
 			border: "4px solid #8cc2f9",
 			value: '60%'
 		},
-    {
-			title: "Wordpress 70%",
+		{
+			title: "Lean 70%",
 			color: "#1b7ee4",
 			border: "4px solid #8cc2f9",
 			value: '70%'
+		},
+		{
+			title: "DevOps 80%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '80%'
+		},
+		{
+			title: "Extreme programming 90%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '90%'
+		},
+    	{
+			title: "Kanban 50%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '50%'
+		},
+    	{
+			title: "Test-driven development 40%",
+			color: "#1b7ee4",
+			border: "4px solid #8cc2f9",
+			value: '40%'
 		}
 	],
   skill3: [
 		{
-			title: "Creative 60%",
+			title: "Docker 70%",
 			color: "#87b615",
 			border: "4px solid #e4f9b1",
-			value: '60%'
+			value: '70%'
 		},
 		{
-			title: "Football 70%",
+			title: "Git 70%",
 			color: "#87b615",
 			border: "4px solid #e4f9b1",
 			value: '70%'
 		},
     {
-			title: "Badminton 85%",
+			title: "Maven 85%",
 			color: "#87b615",
 			border: "4px solid #e4f9b1",
 			value: '85%'
 		},
 		{
-			title: "Leadership 90%",
+			title: "SONAR 60%",
 			color: "#87b615",
 			border: "4px solid #e4f9b1",
-			value: '90%'
+			value: '60%'
+		},
+		{
+			title: "JMeter 30%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '30%'
+		},
+		{
+			title: "Jenkins 60%",
+			color: "#87b615",
+			border: "4px solid #e4f9b1",
+			value: '60%'
 		}
 	]
 }
