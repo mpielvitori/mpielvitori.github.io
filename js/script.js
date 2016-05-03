f15b5212cb0baa15e90d91348d1dee77bec685f0
mpielvitori <martinpielvitori@gmail.com>
2016-05-03 19:46:51 -0300

    Dev

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ $(window).load(function() {
 
 $(document).ready(function() {
 
-  initializeMap(lat,lng);
-  
+	initializeMap(lat,lng);
+
 	//Elements Appear from top
 	$('.item_top').each(function() {
 		$(this).appear(function() {
@@ -36,7 +36,7 @@ $(document).ready(function() {
 			}, 1000);
 		});
 	});
-	
+
 	//Elements Appear from right
 	$('.item_right').each(function() {
 		$(this).appear(function() {
@@ -46,7 +46,7 @@ $(document).ready(function() {
 			}, 1000);
 		});
 	});
-	
+
 	//Elements Appear in fadeIn effect
 	$('.item_fade_in').each(function() {
 		$(this).appear(function() {
@@ -61,237 +61,237 @@ $(document).ready(function() {
 		topSpacing : 0
 	});
 
-  rotate('rotate1');
+	rotate('rotate1');
 
-    // Galleries Slider
+	// Galleries Slider
 	$('.slider_container').flexslider({
-			directionNav: true,
-			controlNav: false
-		});
+		directionNav: true,
+		controlNav: false
+	});
 	/*===================================================================================*/
 	/*  PROFOLIO                                                                         */
 	/*===================================================================================*/
-  	var portfolio = portfolio || {},
+	var portfolio = portfolio || {},
 		$portfolioItems       = $('#portfolio-items'),
 		$filtrable            = $('#portfolio-filter');
 
-		/*===================================================================================*/
-		/*  PROFOLIO INIT FULL WIDTH                                                         */
-		/*===================================================================================*/
-		portfolio.fullWidth = function(){
+	/*===================================================================================*/
+	/*  PROFOLIO INIT FULL WIDTH                                                         */
+	/*===================================================================================*/
+	portfolio.fullWidth = function(){
 
-	        function portfolioCol() {
-	         var winWidth = jQuery(window).width() + 15, columnNumb = 1;
-    if (winWidth > 1024) {
-      columnNumb = 4;
-    } else if (winWidth > 768) {
-      columnNumb = 3;
-    } else if (winWidth > 480) {
-      columnNumb = 2;
-    } else if (winWidth < 480) {
-      columnNumb = 1;
-    }
-    return columnNumb;
-	        }
+		function portfolioCol() {
+			var winWidth = jQuery(window).width() + 15, columnNumb = 1;
+			if (winWidth > 1024) {
+				columnNumb = 4;
+			} else if (winWidth > 768) {
+				columnNumb = 3;
+			} else if (winWidth > 480) {
+				columnNumb = 2;
+			} else if (winWidth < 480) {
+				columnNumb = 1;
+			}
+			return columnNumb;
+		}
 
-	        function setCol() {
+		function setCol() {
 
-	            var width = $(window).width(),
-	                column = portfolioCol(),
-	                articleWidth =  Math.floor(width/column);
+			var width = $(window).width(),
+				column = portfolioCol(),
+				articleWidth =  Math.floor(width/column);
 
-	            $portfolioItems.find('article').each(function () {
-	                $(this).css( {
-	                    width : articleWidth + 'px'
-	                });
-	            });
-	        }
-
-
-	        $(window).load(function(){
-				setCol();
-	            $portfolioItems.isotope({
-					animationEngine: 'best-available',
-					animationOptions: {
-							duration: 250,
-							easing: 'easeInOutSine',
-							queue: false
-				   }
+			$portfolioItems.find('article').each(function () {
+				$(this).css( {
+					width : articleWidth + 'px'
 				});
-	        });
+			});
+		}
 
-			$(window).bind('resize', function () {
-				setCol();
-				$portfolioItems.isotope('reLayout');
+
+		$(window).load(function(){
+			setCol();
+			$portfolioItems.isotope({
+				animationEngine: 'best-available',
+				animationOptions: {
+					duration: 250,
+					easing: 'easeInOutSine',
+					queue: false
+				}
+			});
+		});
+
+		$(window).bind('resize', function () {
+			setCol();
+			$portfolioItems.isotope('reLayout');
+		});
+
+		$filtrable.find('a').click(function(e) {
+			var currentOption = $(this).data('cat');
+
+			$filtrable.find('a').removeClass('active');
+			$(this).addClass('active');
+
+			if (currentOption !== '*') {
+				currentOption = '.' + currentOption;
+			}
+
+			$portfolioItems.isotope({filter : currentOption});
+			return false;
+		});
+
+	};
+
+	/*===================================================================================*/
+	/*  PROFOLIO INIT AJAX                                                               */
+	/*===================================================================================*/
+
+
+	portfolio.ajax = function(){
+
+
+		function portfolioInit() {
+
+			var newHash      = "",
+				$mainContent = $("#portfolio-ajax"),
+				$pageWrap    = $("#portfolio-wrap"),
+				root         = '#!projects/',
+				rootLength   = root.length,
+				url;
+
+			$portfolioItems.find("a").click(function() {
+				window.location.hash = $(this).attr("href");
+				return false;
 			});
 
-	        $filtrable.find('a').click(function(e) {
-	            var currentOption = $(this).data('cat');
+			$(window).bind('hashchange', function(){
 
-	            $filtrable.find('a').removeClass('active');
-	            $(this).addClass('active');
-
-	            if (currentOption !== '*') {
-	            	currentOption = '.' + currentOption;
-	            }
-
-	           	$portfolioItems.isotope({filter : currentOption});
-	            return false;
-	        });
-
-		};
-
-		/*===================================================================================*/
-		/*  PROFOLIO INIT AJAX                                                               */
-		/*===================================================================================*/
-
-
-		portfolio.ajax = function(){
-
-
-			function portfolioInit() {
-
-                var newHash      = "",
-                    $mainContent = $("#portfolio-ajax"),
-                    $pageWrap    = $("#portfolio-wrap"),
-				          	root         = '#!projects/',
-                    rootLength   = root.length,
-                    url;
-
-                $portfolioItems.find("a").click(function() {
-                    window.location.hash = $(this).attr("href");
-                    return false;
-                });
-
-                $(window).bind('hashchange', function(){
-
-					newHash = window.location.hash;
-				    url = newHash.replace(/[#\!]/g, '' );
-                    if (newHash.substr(0,rootLength) == root) {
-                    	if($pageWrap.is(':hidden')){
-                    		$pageWrap.slideDown('3000', function(){});
-                    	}
-						$pageWrap.niceScroll({
-				            cursorcolor:"#666",
-				            cursorwidth:6,
-				            cursorborder: 0,
-				            cursorborderradius: 0
-				        });
-                        $pageWrap.append('<div id="preloader"></div>');
-                        $mainContent.load(url + " .single-portfolio", function(xhr, statusText, request){
-                        	if(statusText == "success"){
-									             setTimeout(function () {
-                                $(".slider_container").flexslider({directionNav: true,controlNav: false	}); 
-                                $('.single-portfolio .media-container').fitVids();
-		                        		$pageWrap.find('#preloader').remove();
-		                            }, 300);
-                            }    
-		                    
-							if(statusText == "error"){
-								$mainContent.html('<div class="row pad-top pad-bottom"><div class="col-md-12 pad-top pad-bottom"><div class="alert-message error"><p>The Content cannot be loaded.</p></div></div></div>');
-		                        $pageWrap.find('#preloader').remove();
-							}
-	                        closeProject();
-	                        nextProject();
-	                        prevProject();
-                       });
-
-	                   $("#portfolio-items article").removeClass("current");
-	                   $("#portfolio-items a[href='" + newHash + "']").parent().addClass("current");
-                     var projectIndex = $('#portfolio-items').find('article.current').index();
-						var projectLength = $('#portfolio-items article').length -1;
-            
-						if(projectIndex == projectLength){
-
-							jQuery('#next-project').addClass('disabled');
-							jQuery('#prev-project').removeClass('disabled');
-
-						}else if(projectIndex == 0){
-
-							jQuery('#prev-project').addClass('disabled');
-							jQuery('#next-project').removeClass('disabled');
-
-						}else{
-							jQuery('#prev-project, #next-project').removeClass('disabled');
-
+				newHash = window.location.hash;
+				url = newHash.replace(/[#\!]/g, '' );
+				if (newHash.substr(0,rootLength) == root) {
+					if($pageWrap.is(':hidden')){
+						$pageWrap.slideDown('3000', function(){});
+					}
+					$pageWrap.niceScroll({
+						cursorcolor:"#666",
+						cursorwidth:6,
+						cursorborder: 0,
+						cursorborderradius: 0
+					});
+					$pageWrap.append('<div id="preloader"></div>');
+					$mainContent.load(url + " .single-portfolio", function(xhr, statusText, request){
+						if(statusText == "success"){
+							setTimeout(function () {
+								$(".slider_container").flexslider({directionNav: true,controlNav: false	});
+								$('.single-portfolio .media-container').fitVids();
+								$pageWrap.find('#preloader').remove();
+							}, 300);
 						}
-                    }
-                    else if(newHash == '')
-                    {
-                    	$('#portfolio-wrap').fadeOut('100', function() {
-                        	$('.single-portfolio').remove();
-                    	});
-                    }
-                });
 
-                $(window).trigger('hashchange');
-            }
+						if(statusText == "error"){
+							$mainContent.html('<div class="row pad-top pad-bottom"><div class="col-md-12 pad-top pad-bottom"><div class="alert-message error"><p>The Content cannot be loaded.</p></div></div></div>');
+							$pageWrap.find('#preloader').remove();
+						}
+						closeProject();
+						nextProject();
+						prevProject();
+					});
+
+					$("#portfolio-items article").removeClass("current");
+					$("#portfolio-items a[href='" + newHash + "']").parent().addClass("current");
+					var projectIndex = $('#portfolio-items').find('article.current').index();
+					var projectLength = $('#portfolio-items article').length -1;
+
+					if(projectIndex == projectLength){
+
+						jQuery('#next-project').addClass('disabled');
+						jQuery('#prev-project').removeClass('disabled');
+
+					}else if(projectIndex == 0){
+
+						jQuery('#prev-project').addClass('disabled');
+						jQuery('#next-project').removeClass('disabled');
+
+					}else{
+						jQuery('#prev-project, #next-project').removeClass('disabled');
+
+					}
+				}
+				else if(newHash == '')
+				{
+					$('#portfolio-wrap').fadeOut('100', function() {
+						$('.single-portfolio').remove();
+					});
+				}
+			});
+
+			$(window).trigger('hashchange');
+		}
 
 
 
-            function closeProject() {
-				     $('#close-project').on('click', function() {
-                    $('#portfolio-wrap').fadeOut('100', function() {
-                        $('.single-portfolio').remove();
-                    });                  
-                    history.pushState('', document.title, window.location.pathname);
-                    window.location.hash = '#_';
-	                return false;
-	            });
-            }
+		function closeProject() {
+			$('#close-project').on('click', function() {
+				$('#portfolio-wrap').fadeOut('100', function() {
+					$('.single-portfolio').remove();
+				});
+				history.pushState('', document.title, window.location.pathname);
+				window.location.hash = '#_';
+				return false;
+			});
+		}
 
-            function nextProject() {
-                $("#next-project").on("click", function() {
-                    $('.single-portfolio').remove();
-                    window.location.hash = $("#portfolio-items .current").next().find('a').attr("href");
-                    return false;
-                });
-            }
+		function nextProject() {
+			$("#next-project").on("click", function() {
+				$('.single-portfolio').remove();
+				window.location.hash = $("#portfolio-items .current").next().find('a').attr("href");
+				return false;
+			});
+		}
 
 
-            function prevProject() {
-                $("#prev-project").on("click", function() {
-                    $('.single-portfolio').remove();
-                    window.location.hash = $("#portfolio-items .current").prev().find('a').attr("href");
-                    return false;
-                });
-            }
+		function prevProject() {
+			$("#prev-project").on("click", function() {
+				$('.single-portfolio').remove();
+				window.location.hash = $("#portfolio-items .current").prev().find('a').attr("href");
+				return false;
+			});
+		}
 
-            if ($portfolioItems.length) {
-                portfolioInit();
-            }
-		};
-		portfolio.fullWidth();
-		portfolio.ajax();
-		
+		if ($portfolioItems.length) {
+			portfolioInit();
+		}
+	};
+	portfolio.fullWidth();
+	portfolio.ajax();
+
 
 	// Contact Form Request
-  $(".validate").validate();	
-  var form = $('#contactform');
-	var submit = $('#contactForm_submit');	
-	var alertx = $('.form-respond'); 
+	$(".validate").validate();
+	var form = $('#contactform');
+	var submit = $('#contactForm_submit');
+	var alertx = $('.form-respond');
 
-  	// form submit event
-    $(document).on('submit', '#contactform', function(e) {
+	// form submit event
+	$(document).on('submit', '#contactform', function(e) {
 		e.preventDefault(); // prevent default form submit
 		// sending ajax request through jQuery
 		$.ajax({
-			url: 'sendemail.php', 
-			type: 'POST', 
+			url: 'https://docs.google.com/forms/d/1mrwqJNt4cucQeBN9QHfUnbIM4RbR6cMJ7w5bUkksVac/formResponse',
+			type: 'POST',
 			dataType: 'html',
-			data: form.serialize(), 
+			data: form.serialize(),
 			beforeSend: function() {
 				alertx.fadeOut();
 				submit.html('Sending....'); // change submit button text
 			},
 			success: function(data) {
 				form.fadeOut(300);
-        alertx.html(data).fadeIn(1000); // fade in response data     
-            setTimeout(function() {
-          alertx.html(data).fadeOut(300);
-          $('#name, #email, #message').val('')
-          form.fadeIn(1800);
-       }, 4000 );  
+				alertx.html(data).fadeIn(1000); // fade in response data
+				setTimeout(function() {
+					alertx.html(data).fadeOut(300);
+					$('#name, #email, #message').val('')
+					form.fadeIn(1800);
+				}, 4000 );
 
 			},
 			error: function(e) {
@@ -310,7 +310,7 @@ $(document).ready(function() {
 		e.stopPropagation();
 	});
 
- 
+
 	//Back To Top
 	$(window).scroll(function() {
 		if ($(window).scrollTop() > 400) {
@@ -328,40 +328,40 @@ $(document).ready(function() {
 });
 
 //Navigation Scrolling
-	$(function() {
-		$('.nav li a').bind('click', function(event) {
-			var $anchor = $(this);
+$(function() {
+	$('.nav li a').bind('click', function(event) {
+		var $anchor = $(this);
 
-			$('html, body').stop().animate({
-				scrollTop : $($anchor.attr('href')).offset().top - 70
-			}, 2000, 'easeInOutExpo');
+		$('html, body').stop().animate({
+			scrollTop : $($anchor.attr('href')).offset().top - 70
+		}, 2000, 'easeInOutExpo');
 
-			event.preventDefault();
-		});
+		event.preventDefault();
 	});
-   
+});
+
 //FullScreen Slider
 $(function(){
-$('#fullscreen-slider').maximage({
-cycleOptions: {
-fx: 'fade',
-speed: 1000,
-timeout: 5000,
-pause: 1
-},
-onFirstImageLoaded: function(){
-jQuery('#cycle-loader').hide();
-jQuery('#fullscreen-slider').fadeIn('slow');
-},
+	$('#fullscreen-slider').maximage({
+		cycleOptions: {
+			fx: 'fade',
+			speed: 1000,
+			timeout: 5000,
+			pause: 1
+		},
+		onFirstImageLoaded: function(){
+			jQuery('#cycle-loader').hide();
+			jQuery('#fullscreen-slider').fadeIn('slow');
+		},
 // cssBackgroundSize might be causing choppiness in retina display safari
-cssBackgroundSize: false
-});
+		cssBackgroundSize: false
+	});
 });
 
 //Parallax
 $(window).bind('load', function() {
-  if(!onMobile)
-	parallaxInit();
+	if(!onMobile)
+		parallaxInit();
 });
 
 function parallaxInit() {
@@ -370,45 +370,45 @@ function parallaxInit() {
 	$('#three-parallax').parallax("50%", 0.2);
 	/*add as necessary*/
 }
-	var onMobile = false;
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-		onMobile = true;
-	}
+var onMobile = false;
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+	onMobile = true;
+}
 
 /*-----------------------------------
-Counter
------------------------------------*/
+ Counter
+ -----------------------------------*/
 
 $(function() {
-  "use strict";
-  $(".number-counters").appear(function(){
-  $(".number-counters [data-to]").each(function(){
-      var count = $(this).attr('data-to');
-  $(this).delay(6000).countTo({
-        from: 50,
-        to: count,
-        speed: 3000,
-        refreshInterval: 50,  
-      });  
-    });
-  });
+	"use strict";
+	$(".number-counters").appear(function(){
+		$(".number-counters [data-to]").each(function(){
+			var count = $(this).attr('data-to');
+			$(this).delay(6000).countTo({
+				from: 50,
+				to: count,
+				speed: 3000,
+				refreshInterval: 50,
+			});
+		});
+	});
 });
 
 //Initilize Google Map
- function initializeMap(lat,lng) {
-     var mapOptions = {
-       center: new google.maps.LatLng(lat, lng),
-       zoom: 11,
-       zoomControl: true,
-       scaleControl: false,
-       scrollwheel: false,
-       mapTypeId: google.maps.MapTypeId.ROADMAP
-     };
-     var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-     var marker = new google.maps.Marker({
-     position: mapOptions['center'],
-     map: map,
-     });
-       
-     return map;
- }    
+function initializeMap(lat,lng) {
+	var mapOptions = {
+		center: new google.maps.LatLng(lat, lng),
+		zoom: 11,
+		zoomControl: true,
+		scaleControl: false,
+		scrollwheel: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	var marker = new google.maps.Marker({
+		position: mapOptions['center'],
+		map: map,
+	});
+
+	return map;
+}
