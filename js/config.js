300438dfc8348df74599a0dfd45f488a7816244f
Martín Pielvitori <martinpielvitori@gmail.com>
2019-11-23 21:49:00 -0300

    Add softvision experience

--- js/config.js
+++ js/config.js
@@ -1,104 +1,104 @@
-/* Google MAP Settings*/
-  var lat= -34.604;  //Change the value with your address Latitude
-  var lng= -58.382;  //Change the value with your address Longitude
+/* Google MAP Settings */
+const lat = -34.604; // Change the value with your address Latitude
+const lng = -58.382; // Change the value with your address Longitude
 
-//Skill Chart Data
-var skillData = {
-	skill1: [
+// Skill Chart Data
+const skillData = {
+  skill1: [
     {
-      title: "NodeJS 80%",
-      color: "#7ba036",
-      border: "4px solid #a7ce5f",
-      value: '80%'
+      title: 'NodeJS 90%',
+      color: '#7ba036',
+      border: '4px solid #a7ce5f',
+      value: '90%',
     },
     {
-			title: "JAVA EE 90%",
-			color: "#7ba036",
-			border: "4px solid #a7ce5f",
-			value: '90%'
-		},
-		{
-			title: "SQL / NoSQL 70%",
-			color: "#7ba036",
-			border: "4px solid #a7ce5f",
-			value: '70%'
-		},
-    {
-			title: "Angular 60%",
-			color: "#7ba036",
-			border: "4px solid #a7ce5f",
-			value: '60%'
-		},
-    {
-			title: "Android 60%",
-			color: "#7ba036",
-			border: "4px solid #a7ce5f",
-			value: '60%'
-		}
-
-	],
-	skill2: [
-		{
-			title: "Agile/Scrum 90%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '90%'
-		},
-    {
-			title: "DevOps 80%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '80%'
-		},
-    {
-      title: "TDD 70%",
-      color: "#1b7ee4",
-      border: "4px solid #8cc2f9",
-      value: '70%'
+      title: 'JAVA EE 90%',
+      color: '#7ba036',
+      border: '4px solid #a7ce5f',
+      value: '90%',
     },
-		{
-			title: "Lean 70%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '70%'
-		},
+    {
+      title: 'SQL / NoSQL 70%',
+      color: '#7ba036',
+      border: '4px solid #a7ce5f',
+      value: '70%',
+    },
+    {
+      title: 'Angular 60%',
+      color: '#7ba036',
+      border: '4px solid #a7ce5f',
+      value: '60%',
+    },
+    {
+      title: 'Android 60%',
+      color: '#7ba036',
+      border: '4px solid #a7ce5f',
+      value: '60%',
+    },
+
+  ],
+  skill2: [
+    {
+      title: 'Agile/Scrum 90%',
+      color: '#1b7ee4',
+      border: '4px solid #8cc2f9',
+      value: '90%',
+    },
+    {
+      title: 'DevOps 80%',
+      color: '#1b7ee4',
+      border: '4px solid #8cc2f9',
+      value: '80%',
+    },
+    {
+      title: 'TDD 70%',
+      color: '#1b7ee4',
+      border: '4px solid #8cc2f9',
+      value: '70%',
+    },
+    {
+      title: 'Lean 70%',
+      color: '#1b7ee4',
+      border: '4px solid #8cc2f9',
+      value: '70%',
+    },
   	{
-			title: "Kanban 80%",
-			color: "#1b7ee4",
-			border: "4px solid #8cc2f9",
-			value: '80%'
-		}
-	],
+      title: 'Kanban 80%',
+      color: '#1b7ee4',
+      border: '4px solid #8cc2f9',
+      value: '80%',
+    },
+  ],
   skill3: [
-		{
-			title: "Docker 80%",
-			color: "#804caa",
-			border: "4px solid #b37be0",
-			value: '80%'
-		},
-		{
-			title: "Git 80%",
-			color: "#804caa",
-			border: "4px solid #b37be0",
-			value: '80%'
-		},
     {
-			title: "Maven 85%",
-			color: "#804caa",
-			border: "4px solid #b37be0",
-			value: '85%'
-		},
-		{
-			title: "AWS 60%",
-			color: "#804caa",
-			border: "4px solid #b37be0",
-			value: '60%'
-		},
-		{
-			title: "ElasticSearch 60%",
-			color: "#804caa",
-			border: "4px solid #b37be0",
-			value: '60%'
-		}
-	]
-}
+      title: 'Docker 80%',
+      color: '#804caa',
+      border: '4px solid #b37be0',
+      value: '80%',
+    },
+    {
+      title: 'Git 90%',
+      color: '#804caa',
+      border: '4px solid #b37be0',
+      value: '90%',
+    },
+    {
+      title: 'K8s 60%',
+      color: '#804caa',
+      border: '4px solid #b37be0',
+      value: '60%',
+    },
+    {
+      title: 'Istio 60%',
+      color: '#804caa',
+      border: '4px solid #b37be0',
+      value: '60%',
+    },
+    {
+      title: 'Terraform 40%',
+      color: '#804caa',
+      border: '4px solid #b37be0',
+      value: '40%',
+    },
+  ],
+};
